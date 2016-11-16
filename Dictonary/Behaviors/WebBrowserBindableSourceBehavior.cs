226e5f9aa813ab149927b4cf39bed5ae1af5c773
Simon Pilkington <professor.twat@gmail.com>
2016-11-16 16:01:31 +0100

    More sensible UI layout. Move WebBrowser bindable source to behaviour.

---
+++ Dictonary/Behaviors/WebBrowserBindableSourceBehavior.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Interactivity;
+
+namespace Dictonary.Behaviors
+{
+	public class WebBrowserBindableSourceBehavior : Behavior<WebBrowser>
+	{
+		private bool _isAttached;
+
+		public string BindableSource
+		{
+			get { return (string)GetValue(BindableSourceProperty); }
+			set { SetValue(BindableSourceProperty, value); }
+		}
+		
+		public static readonly DependencyProperty BindableSourceProperty =
+			DependencyProperty.Register(nameof(BindableSource), typeof(string), typeof(WebBrowserBindableSourceBehavior), new PropertyMetadata(BindableSourceChanged));
+
+		protected override void OnAttached()
+		{
+			base.OnAttached();
+			_isAttached = true;
+		}
+
+		private static void BindableSourceChanged(object sender, DependencyPropertyChangedEventArgs e)
+		{
+			var dependencyObject = (WebBrowserBindableSourceBehavior)sender;
+
+			if (!dependencyObject._isAttached)
+				return;
+
+			WebBrowser associatedObject = dependencyObject.AssociatedObject;
+			associatedObject.Source = new Uri((string)e.NewValue);
+		}
+	}
+}
